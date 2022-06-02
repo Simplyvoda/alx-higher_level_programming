cca8a68496e4cf349c2382ebc34b73f67c7f162a
Simplyvoda <efem.vodina@gmail.com>
2022-06-02 14:24:29 +0100

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-i = 1
-if len(sys.argv)-1 == 0:
-    print("0 arguments.")
-elif len(sys.argv)-1 == 1:
-    print("1 argument:")
-else :
-    print("{} arguments:".format(len(sys.argv)-1))
-for arg in sys.argv[1:]:
-    print("{}: {}".format(i, str(sys.argv[i])))
-    i = 1 + i
+if __name__ = "__main__":
+    from sys import argv
+    i = 1
+    if len(argv)-1 == 0:
+        print("0 arguments.")
+    elif len(argv)-1 == 1:
+        print("1 argument:")
+    else :
+        print("{:d} arguments:".format(len(argv)-1))
+    for arg in argv[1:]:
+        print("{:d}: {:s}".format(i, arg))
+        i = 1 + i
