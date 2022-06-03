4a947c6a75f620f19fc683b13586045f50b2c63f
Vodina Efem <efem.vodina@gmail.com>
2022-06-02 17:12:51 -0700

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif len(argv)-1 == 1:
         print("1 argument:")
-    else :
+    else:
         print("{:d} arguments:".format(len(argv)-1))
     for arg in argv[1:]:
         print("{:d}: {:s}".format(i, arg))
