41d47048c8bf1ad57d173350471b4efa907220c6
Simplyvoda <efem.vodina@gmail.com>
2022-06-03 01:34:51 +0100

    commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ = "__main__":
     from sys import argv
     sum = 0
-    for arg in argv[1:]
-    sum += int(arg)
+    for arg in argv[1:]:
+        sum += int(arg)
     print("{:d}".format(sum))
