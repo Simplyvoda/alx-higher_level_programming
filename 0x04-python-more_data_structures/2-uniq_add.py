fa5289d75a3f64d78c22af95dea1ee3742580f05
Vodina Efem <efem.vodina@gmail.com>
2022-06-08 08:03:19 -0700

    reverting to my solution

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    newlist = set(my_list)
-
+    newlist = []
+    for i in my_list:
+        if i not in newlist:
+            newlist.append(i)
     sum = 0
     for x in newlist:
         sum = sum + x
