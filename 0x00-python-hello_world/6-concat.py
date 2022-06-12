8d346c471576638fa2638be31ed80a084a09eeec
Simplyvoda <efem.vodina@gmail.com>
2022-06-12 15:45:35 +0100

    commit

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-userdictionary = {'str1': 'Holberton', 'str2': 'School'}
-print(f"Welcome to {userdictionary['str1']} {userdictionary['str2']}!")
+str3 = "{} {}".format(str1, str2)
+print("Welcome to {}!".format(str3))
