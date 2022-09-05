7f9c9f3c6e472efe2ef80290cd7b085d9ec5cc50
Vodina Efem <efem.vodina@gmail.com>
2022-09-05 14:06:27 +0000

    commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const process = require('process');
 
-if (process.argv > 2) {
-    if (process.argv.length === 3) {
-        console.log('Argument found');
-    } else {
-        console.log('Arguments found');
-    }
+if (process.argv.length > 2) {
+  if (process.argv.length === 3) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
 } else {
-    console.log('No argument');
+  console.log('No argument');
 }
