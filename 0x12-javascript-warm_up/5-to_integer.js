a87f5dad01b07dd1c9fd3a0d74d22cbac07c1702
Vodina Efem <efem.vodina@gmail.com>
2022-09-05 15:29:36 +0000

    commit

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const process = require('process');
-let a = process.argv[2];
+const a = process.argv[2];
 
-if (a/10) {
-	console.log('My number:' + ' ' + Math.floor(a));
+if (a / 10) {
+  console.log('My number:' + ' ' + Math.floor(a));
 } else {
-	console.log('Not a number');
+  console.log('Not a number');
 }
