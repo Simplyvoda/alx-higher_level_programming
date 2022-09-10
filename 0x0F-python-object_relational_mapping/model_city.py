91ed84880a1845648e49c3564ed74e643b0baed5
Simplyvoda <efem.vodina@gmail.com>
2022-09-10 19:55:49 +0100

    commit

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+contains the city class
+"""
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """Represents city class"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
