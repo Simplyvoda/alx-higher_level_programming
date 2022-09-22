a282abd22673c103f70f25d7c7967cfd35de86e8
Simplyvoda <efem.vodina@gmail.com>
2022-09-22 18:38:57 +0100

    commit

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+this module contains a function that finds the peak in a list of unsorted integers
+"""
+
+def find_peak(list_of_integers):
+    """function to find peak , uses binary search"""
+    i = 0
+    j = len(list_of_integers) - 1
+    while i <= j:
+        m = (i + j)/2
+        if list_of_integers[m] > list_of_integers[m+1]:
+            j = m
+        else:
+            i = m + 1
+    return i
