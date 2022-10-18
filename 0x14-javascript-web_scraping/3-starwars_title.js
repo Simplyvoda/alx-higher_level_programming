d0a343b545d766adb24381f5c39afe5f2826c84b
Vodina Efem <efem.vodina@gmail.com>
2022-10-18 19:26:56 +0000

    commit

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/*
+prints the title of a Star wars movie where episode
+number matched integer
+*/
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const id = process.argv[2];
+
+
+request.get(url + id, function (err, res, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
