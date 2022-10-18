aed84696bbde6c422c8cfee940c595ca125aec17
Vodina Efem <efem.vodina@gmail.com>
2022-10-18 08:02:59 +0000

    commit

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+This script reads and prints content of a file
+*/
+const fs = require('fs');
+const process = require('process');
+
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
